5fce4150d91b944b290793753b0b38368456b325
jrchen <jrchen88@mitac.com.tw>
2023-03-20 15:10:39 +0800

    React Practice - Untitled PWA Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Offline, Online } from "react-detect-offline";
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a>
           Learn React
         </a>
-      </header>
+            <Online>Only shown Online (surprise!)</Online>
+            <Offline>Only shown offline (surprise!)</Offline>
+        </header>
     </div>
   );
 }
