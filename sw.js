08498e2f3ce8df1221c03d241da296879308d8c3
jrchen <jrchen478@gmail.com>
2023-03-20 15:49:51 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,32 @@
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources =
+    ['/', '/index.html', '/favicon.ico', '/logo192.png', '/logo512.png', '/manifest.json'
+        ,'./src/App.css','./src/index.css','./src/logo.svg'];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }),
+    );
+});
